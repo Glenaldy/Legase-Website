67c9c795f4430c5b43e073d6b7c934671a941f3b
Glenaldy Sebastian <glenaldy98@gmail.com>
2020-11-13 17:00:25 +0900

    added banner and hover functionality

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,27 @@
 $(function () {
   $(".hide-load").hide();
+  $(".hover-show").css({ opacity: "0" });
   $(".expander").on("mouseover", HoverInExpand).on("mouseout", HoverOutExpand);
+  $(".banner").on("mouseover", HoverInBanner).on("mouseout", HoverOutBanner);
+
+  function HoverInBanner() {
+    let target = $(this);
+    target.find(".hover-show").stop(true).transition({ opacity: 1 });
+    target.find(".hover-hide").stop(true).transition({ opacity: 0 });
+    target.find(".banner-title").css({ "text-decoration": "underline" });
+    target.find(".banner-background img").stop(true).transition({ opacity: 1 });
+  }
+
+  function HoverOutBanner() {
+    let target = $(this);
+    target.find(".hover-hide").stop(true).transition({ opacity: 1 });
+    target.find(".hover-show").stop(true).transition({ opacity: 0 });
+    target.find(".banner-title").css({ "text-decoration": "none" });
+    target
+      .find(".banner-background img")
+      .stop(true)
+      .transition({ opacity: 0.5 });
+  }
 });
 function HoverInExpand() {
   $(this).find(".expanding").show();
